eb464d58040c0ab2499ca009aac0b60105e13272
Aaron Willcox <57405769+AaronWillcox@users.noreply.github.com>
2022-06-01 11:44:08 +0100

    Add identity scaff

---
+++ Migrations/20220531204730_init2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sweet_Management_Tool.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
